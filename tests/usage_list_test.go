a2cc71b8cfa2ba2f26dafa2b84bb737f6b7ecad8
Javier Parada <javierparadadev@gmail.com>
2022-05-07 12:55:33 +0200

    add lists tests

--- tests/usage_list_test.go
+++ tests/usage_list_test.go
@@ -11,3 +11,33 @@ func TestFirstValueOfList(t *testing.T) {
 	result := deepfinder.DeepFind(data, "0")
 	assert.EqualValues(t, "a", result)
 }
+
+func TestSecondValueOfList(t *testing.T) {
+	data := []interface{}{"a", "b", "c"}
+	result := deepfinder.DeepFind(data, "1")
+	assert.EqualValues(t, "b", result)
+}
+
+func TestLastValueOfList(t *testing.T) {
+	data := []interface{}{"a", "b", "c"}
+	result := deepfinder.DeepFind(data, "2")
+	assert.EqualValues(t, "c", result)
+}
+
+func TestNonNumericAccessOfList(t *testing.T) {
+	data := []interface{}{"a", "b", "c"}
+	result := deepfinder.DeepFind(data, "b")
+	assert.EqualValues(t, nil, result)
+}
+
+func TestNonHasheableKeyAccessOfList(t *testing.T) {
+	data := []interface{}{"a", "b", "c"}
+	result := deepfinder.DeepFind(data, "[]")
+	assert.EqualValues(t, nil, result)
+}
+
+func TestEmbeddedList(t *testing.T) {
+	data := map[string]interface{}{"values": []interface{}{"a", "b", "c"}}
+	result := deepfinder.DeepFind(data, "values.1")
+	assert.EqualValues(t, "b", result)
+}
